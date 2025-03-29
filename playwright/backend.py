b3298d31456b65874bb6232558b291a61ddb545a
AnnaV4 <101451874+AnnaV4@users.noreply.github.com>
2025-03-29 12:44:09 +0000

    locust+playwright

---
+++ playwright/backend.py
@@ -0,0 +1,30 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # Start tracing before creating / navigating a page.
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+# Stop tracing and export it into a zip archive.
+    
+    
+    page = context.new_page()
+    page.goto("https://ubiquitous-train-9gxjr557wwxfpqg6-8000.app.github.dev/admin/login/?next=/admin/")
+    page.get_by_role("textbox", name="Username:").click()
+    page.get_by_role("textbox", name="Username:").fill("admin")
+    page.get_by_role("textbox", name="Username:").press("Enter")
+    page.get_by_role("textbox", name="Password:").click()
+    page.get_by_role("textbox", name="Password:").fill("123987")
+    page.goto("https://ubiquitous-train-9gxjr557wwxfpqg6-8000.app.github.dev/nocodb_data")
+    # ---------------------
+
+    context.tracing.stop(path = "playwright/trace.zip")
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
